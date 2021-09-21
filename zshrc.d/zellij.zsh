cd07f0e8d75873b9bf5ba311546feb49e111821e
Josh Jones <ohblonddev@gmail.com>
2021-09-21 18:04:41 +0100

    Minor refactor

--- zshrc.d/zellij.zsh
+++ zshrc.d/zellij.zsh
@@ -2,6 +2,6 @@ if [ -f "${HOME}/.config/zellij/config.yml" ]; then
   export ZELLIJ_CONFIG_FILE="${HOME}/.config/zellij/config.yml"
 fi
 
-if hash zellij 2>/dev/null; then
+if [ "$(command -v zellij)" ]; then
     alias za='zellij attach || zellij'
 fi
