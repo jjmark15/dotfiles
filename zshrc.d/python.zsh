f9224e9d644fb2147306325e43a4998ef25181ee
Josh Jones <ohblonddev@gmail.com>
2022-01-05 22:24:11 +0000

    Fix pyenv env var

--- zshrc.d/python.zsh
+++ zshrc.d/python.zsh
@@ -2,7 +2,7 @@ PYENV_BIN_DIR="${HOME}/.pyenv/bin"
 
 if [ -d "$PYENV_BIN_DIR" ]; then
   export PIPENV_VENV_IN_PROJECT="true"
-  export PYENV_VIRTUALENV_DISABLE_PROMP=2
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
   export PATH="$PYENV_BIN_DIR:$PATH"
 
   activate_venv() {
