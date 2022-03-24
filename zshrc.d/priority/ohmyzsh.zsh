83f3321c46058610562bacec674d4a31c92cf963
Josh Jones <ohblonddev@gmail.com>
2022-03-24 16:50:35 +0000

    conditionally source ohmyzsh

--- zshrc.d/priority/ohmyzsh.zsh
+++ zshrc.d/priority/ohmyzsh.zsh
@@ -1,29 +1,32 @@
 export ZSH="${HOME}/.oh-my-zsh"
 
-ZSH_THEME="robbyrussell"
+if [ -f "$ZSH/oh-my-zsh.sh" ]; then
 
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
+  ZSH_THEME="robbyrussell"
 
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+  # Uncomment the following line to use case-sensitive completion.
+  # CASE_SENSITIVE="true"
 
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+  # Uncomment the following line to use hyphen-insensitive completion.
+  # Case-sensitive completion must be off. _ and - will be interchangeable.
+  # HYPHEN_INSENSITIVE="true"
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+  # Uncomment the following line to enable command auto-correction.
+  # ENABLE_CORRECTION="true"
 
-plugins=(
-  git
-  fzf
-  zsh-syntax-highlighting
-  zsh-autosuggestions
-  kubectl
-  rust
-)
+  # Uncomment the following line if you want to disable marking untracked files
+  # under VCS as dirty. This makes repository status check for large repositories
+  # much, much faster.
+  # DISABLE_UNTRACKED_FILES_DIRTY="true"
 
-source $ZSH/oh-my-zsh.sh
+  plugins=(
+    git
+    fzf
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+    kubectl
+    rust
+  )
+
+  source "$ZSH/oh-my-zsh.sh"
+fi
