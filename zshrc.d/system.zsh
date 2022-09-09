c88ec0e53cebd26e91e3d61c32146b4b0e9a9cc4
Josh Jones <ohblonddev@gmail.com>
2022-09-09 18:00:26 +0100

    Add kill-port function

--- zshrc.d/system.zsh
+++ zshrc.d/system.zsh
@@ -1,18 +1,16 @@
 function kill-port() {
-    PORT=$1
+  PORT=$1
 
-    if [ -z "$PORT" ];
-    then
-        echo "PORT is a mandatory argument"
-        return
-    fi
+  if [ -z "$PORT" ]; then
+    echo "PORT is a mandatory argument"
+    return
+  fi
 
-    PID="$(lsof -t -i:"${PORT}")"
-    if [ -z "$PID" ];
-    then
-        echo "No processes found on port ${PORT}"
-        return
-    fi
+  PID="$(lsof -t -i:"${PORT}")"
+  if [ -z "$PID" ]; then
+    echo "No processes found on port ${PORT}"
+    return
+  fi
 
-    kill -9 "$PID"
-  }
+  kill -9 "$PID"
+}
