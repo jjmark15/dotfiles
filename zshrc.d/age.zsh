1ed21e0a9e74acb3577bf4f0e8393177e27ef5e7
Josh Jones <ohblonddev@gmail.com>
2022-11-03 19:21:27 +0000

    Fix age if statement

--- zshrc.d/age.zsh
+++ zshrc.d/age.zsh
@@ -1,7 +1,7 @@
 if path_has_bins age 2>/dev/null; then
   export AGE_DIR=~/.age
 
-  if path_has_bins rg sd; then
+  if path_has_bins rg sd 2>/dev/null; then
     AGE_PUB_KEY=$(rg public\ key ${AGE_DIR}/key.txt | sd "# public key: " "")
   fi
 fi
