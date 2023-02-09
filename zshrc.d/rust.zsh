11ca919f4b48a84c71855d6384bb92d95f3e5d31
Josh Jones <ohblonddev@gmail.com>
2023-02-09 19:35:21 +0000

    Add nextest shortcuts

--- zshrc.d/rust.zsh
+++ zshrc.d/rust.zsh
@@ -1,3 +1,8 @@
 if [ -d "$HOME/.cargo/bin" ]; then
   addtopath "$HOME/.cargo/bin"
+
+  if [ "$(command -v direnv)" ]; then
+    alias cnt="cargo nextest"
+    alias cntr="cargo nextest run"
+  fi
 fi
