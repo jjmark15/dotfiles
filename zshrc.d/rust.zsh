1ac17d31874eddf81e881687b2b68b3888172fbe
Josh Jones <ohblonddev@gmail.com>
2023-03-10 18:04:22 +0000

    Add nexttest alias

--- zshrc.d/rust.zsh
+++ zshrc.d/rust.zsh
@@ -2,6 +2,7 @@ if [ -d "$HOME/.cargo/bin" ]; then
   addtopath "$HOME/.cargo/bin"
 
   if command_exists direnv; then
+    alias ct="cargo nextest"
     alias cnt="cargo nextest"
     alias cntr="cargo nextest run"
   fi
