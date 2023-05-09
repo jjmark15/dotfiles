f34464502e13adc5fe45c5ce66b4b7fd56d757e7
Josh Jones <ohblonddev@gmail.com>
2023-05-09 09:15:42 +0100

    Use new nix cli

--- zshrc.d/priority/nixos.zsh
+++ zshrc.d/priority/nixos.zsh
@@ -1,7 +1,7 @@
 if [ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ]; then . "$HOME"/.nix-profile/etc/profile.d/nix.sh; fi
 
-if command_exists nix-env; then
+if command_exists nix; then
   quick-update-nixos() {
-      nix-channel --update && nix-env -u '*' && nix-collect-garbage -d
+      nix profile upgrade '.*' && nix-collect-garbage
   }
 fi
