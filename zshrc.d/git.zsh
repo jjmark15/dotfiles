bbb698465ccdcfbe59e6569fb2770cb8bd3ffcac
Josh Jones <ohblonddev@gmail.com>
2024-09-10 21:21:02 +0100

    Add interactive git reset to reflog entry

--- zshrc.d/git.zsh
+++ zshrc.d/git.zsh
@@ -1,23 +1,15 @@
-export GIT_DUET_GLOBAL=true
-export GIT_DUET_CO_AUTHORED_BY=1
+git_reflog_interactive_reset() {
+  local selected_ref
 
-alias gitpruneremote='git remote prune origin'
+  selected_ref=$(git reflog --color=always | \
+    fzf --ansi --no-sort --reverse --tiebreak=index \
+        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" | \
+    rg -o 'HEAD@\{\d\}' | head -n 1)
 
-shouldirebase() {
-  CHANGED=$(git status --porcelain)
-  if [ -n "${CHANGED}" ]; then
-    echo 'repo has changes - aborting.';
-  else
-    basebranch=${1:-master}
-    git checkout $basebranch > /dev/null 2>&1 && git pull > /dev/null 2>&1 && git checkout - > /dev/null 2>&1
-    brnch=$(git branch | grep \* | cut -d ' ' -f2)
-    hash1=$(git show-ref --heads -s ${basebranch})
-    hash2=$(git merge-base ${basebranch} ${brnch})
-    [ "${hash1}" = "${hash2}" ] && echo "Rebase required: false" || echo "Rebase required: true"
+  if [[ -z ${selected_ref} ]]; then
+    echo "Ref not selected"
+    return 1
   fi
-}
 
-gitsquashsincemaster() {
-  commits_since_master=$(git rev-list --count HEAD ^master)
-  git reset --soft HEAD~${commits_since_master} && git commit --edit -m "$(git log --format=%B --reverse HEAD..HEAD@{1})";
+  git reset --hard "${selected_ref}"
 }
