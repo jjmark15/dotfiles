b6d9406d31e4927a98dde3c9b8d7a253eb469562
Josh Jones <ohblonddev@gmail.com>
2024-09-10 22:12:25 +0100

    Reduce preview output of interactive git reset functions

--- zshrc.d/git.zsh
+++ zshrc.d/git.zsh
@@ -3,7 +3,7 @@ git_reflog_interactive_reset() {
 
   selected_ref=$(git reflog --color=always | \
     fzf --ansi --no-sort --reverse --tiebreak=index \
-        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" | \
+        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always --name-only --pretty=medium %'" | \
     rg -o 'HEAD@\{\d\}' | head -n 1)
 
   if [[ -z ${selected_ref} ]]; then
@@ -19,7 +19,7 @@ git_log_interactive_reset() {
 
   selected_ref=$(git log --oneline --decorate --color=always | \
     fzf --ansi --no-sort --reverse --tiebreak=index \
-        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" | \
+        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always --name-only --pretty=medium %'" | \
     rg -o '^[a-z0-9]{7}' | head -n 1)
 
   if [[ -z ${selected_ref} ]]; then
