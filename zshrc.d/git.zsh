43a0283d623d21cbc2ad68b8f4941ce939be45ed
Josh Jones <ohblonddev@gmail.com>
2024-09-10 21:44:44 +0100

    Add interactive git reset to log entry

--- zshrc.d/git.zsh
+++ zshrc.d/git.zsh
@@ -13,3 +13,19 @@ git_reflog_interactive_reset() {
 
   git reset --hard "${selected_ref}"
 }
+
+git_log_interactive_reset() {
+  local selected_ref
+
+  selected_ref=$(git log --oneline --decorate --color=always | \
+    fzf --ansi --no-sort --reverse --tiebreak=index \
+        --preview "echo {} | grep -o '[a-z0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always %'" | \
+    rg -o '^[a-z0-9]{7}' | head -n 1)
+
+  if [[ -z ${selected_ref} ]]; then
+    echo "Ref not selected"
+    return 1
+  fi
+
+  git reset --hard "${selected_ref}"
+}
